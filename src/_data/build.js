0fed2524814e3ef2947ea627a735dddda3907e64
andystevenson <andystevenson@mac.com>
2022-01-24 15:47:41 +0000

    linked to @andystevenson/11ty package

--- src/_data/build.js
+++
@@ -1,6 +0,0 @@
-const build = {
-  development: process.env.BUILD === 'development' || !process.env.BUILD,
-  production: process.env.BUILD === 'production',
-}
-
-module.exports = build
--- src/_data/meta.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title: 'West Warwickshire',
-  description:
-    'West Warwickshire Sports Complex - the heart of sport & hospitality in Solihull.',
-  company: 'West Warwickshire Sports Complex',
-  site: 'https://www.westwarwicks.club',
-}
