b02b4eb2758fb940f727575c6a5f9b8c763b1e56
andystevenson <andystevenson@mac.com>
2022-01-26 18:57:18 +0000

    moved sass into its own library

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
-import postcssJitProps from 'postcss-jit-props'
-import postcssCustomMedia from 'postcss-custom-media'
 
-import OpenProps from 'open-props'
-
-export default defineConfig({
-  // // ...
-  // css: {
-  //   postcss: {
-  //     plugins: [postcssJitProps(OpenProps), postcssCustomMedia()],
-  //   },
-  // },
-})
+export default defineConfig({})
