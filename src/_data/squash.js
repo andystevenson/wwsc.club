907fe049b70d741fceb1aa7ae164bdec07545bea
andystevenson <andystevenson@mac.com>
2022-02-28 18:00:40 +0000

    made a start on squash

---
+++ src/_data/squash.js
@@ -0,0 +1,28 @@
+module.exports = {
+  affiliates: [
+    {
+      name: 'squash england',
+      website: 'https://www.englandsquash.com/',
+      logo: '/files/squash-england.png',
+    },
+    {
+      name: 'psa world tour',
+      website: 'https://psaworldtour.com/',
+      logo: '/files/psa-world-tour-colour.png',
+    },
+    {
+      name: 'psa squash tv',
+      website: 'https://psaworldtour.com/tv',
+      logo: '/files/stv-monotone.png',
+    },
+    {
+      name: 'squashskills',
+      website: 'https://squashskills.com/',
+      logo: '/files/squashskills-logo.png',
+    },
+  ],
+  social: [
+    { youtube: 'https://www.youtube.com/psasquashtv' },
+    { facebook: 'https://www.facebook.com/PSAworldtour' },
+  ],
+}
