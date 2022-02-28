cb8ce353d5790078aca671db9cf824326f8f2410
andystevenson <andystevenson@mac.com>
2022-02-28 16:28:42 +0000

    top level links and refactored links

---
+++ src/_data/club.js
@@ -0,0 +1,34 @@
+module.exports = {
+  affiliates: [
+    {
+      name: 'sport england',
+      website: 'https://www.sportengland.org/',
+      logo: '/files/sport-england-logo.png',
+    },
+    {
+      name: 'lawn tennis association',
+      website: 'https://www.lta.org.uk/',
+      logo: '/files/lta-logo.svg',
+    },
+    {
+      name: 'england football',
+      website: 'https://www.englandfootball.com/',
+      logo: '/files/england-football-logo.svg',
+    },
+    {
+      name: 'england cricket board',
+      website: 'https://www.ecb.co.uk/',
+      logo: '/files/ecb.co.uk.png',
+    },
+    {
+      name: 'england hockey',
+      website: 'https://www.englandhockey.co.uk/',
+      logo: '/files/england-hockey-logo.svg',
+    },
+    {
+      name: 'squash england',
+      website: 'https://www.englandsquash.com/',
+      logo: '/files/squash-england.png',
+    },
+  ],
+}
