bdcb31141e41ee329b16d0359f5a790d83005f80
andystevenson <andystevenson@mac.com>
2022-03-01 22:49:48 +0000

    added basic bar information

---
+++ src/_data/bar.js
@@ -0,0 +1,16 @@
+module.exports = {
+  affiliates: [
+    {
+      name: 'restaurant guide',
+      website:
+        'https://restaurantguru.com/West-Warwickshire-Sports-Complex-Solihull',
+      logo: '/files/restaurant-guru-logo.svg',
+    },
+    {
+      name: 'food standards agency',
+      website:
+        'https://ratings.food.gov.uk/business/en-GB/524499/West-Warwickshire-Sports-Club-Olton',
+      logo: '/files/food-standards-5star.jpeg ',
+    },
+  ],
+}
