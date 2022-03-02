c178070d218cc882f1e18400b811256a563fc732
andystevenson <andystevenson@mac.com>
2022-03-02 14:38:57 +0000

    added preliminary racketball and more on join page

---
+++ src/_data/racketball.js
@@ -0,0 +1,19 @@
+module.exports = {
+  affiliates: [
+    {
+      name: 'squash england',
+      website: 'https://www.englandsquash.com/',
+      logo: '/files/squash-england.png',
+    },
+    {
+      name: 'uk racketball',
+      website: 'https://www.uk-racketball.com/',
+      logo: '/files/racketball-uk-logo.png',
+    },
+  ],
+  social: [
+    { facebook: 'https://www.facebook.com/UKRacketballSeries' },
+    { twitter: 'https://twitter.com/uk_racketball' },
+    { instagram: 'https://www.instagram.com/ukracketball/' },
+  ],
+}
--- src/js/join.js
+++ src/js/join.js
@@ -1,8 +1,11 @@
 console.log('join join join join')
 
+const main = document.querySelector('main')
 const annual = document.getElementById('annual-prices')
 const monthly = document.getElementById('monthly-prices')
 const visitor = document.getElementById('visitor-prices')
+const benefits = main.querySelector('section.benefits')
+const withMembership = main.querySelector('span.with-membership')
 
 // monthly prices are not shown by default
 const annualIcon = annual.firstElementChild
@@ -19,15 +22,19 @@ monthlyPrices.forEach((price) => (price.style.display = 'none'))
 visitorPrices.forEach((price) => (price.style.display = 'none'))
 
 annual.addEventListener('click', (e) => {
+  withMembership.style.display = 'inline'
   visitorIcon.style.display = 'none'
   monthlyIcon.style.display = 'none'
   annualIcon.style.display = 'block'
+
   visitorPrices.forEach((price) => (price.style.display = 'none'))
   monthlyPrices.forEach((price) => (price.style.display = 'none'))
   annualPrices.forEach((price) => (price.style.display = 'grid'))
 })
 
 monthly.addEventListener('click', (e) => {
+  withMembership.style.display = 'inline'
+  benefits.style.display = 'grid'
   visitorIcon.style.display = 'none'
   annualIcon.style.display = 'none'
   monthlyIcon.style.display = 'block'
@@ -37,6 +44,8 @@ monthly.addEventListener('click', (e) => {
 })
 
 visitor.addEventListener('click', (e) => {
+  withMembership.style.display = 'none'
+  benefits.style.display = 'none'
   visitorIcon.style.display = 'block'
   annualIcon.style.display = 'none'
   monthlyIcon.style.display = 'none'
