44b681fb5c64644113022f255f035b67161a6cbb
andystevenson <andystevenson@mac.com>
2022-03-07 18:01:41 +0000

    fleshed out squash pages

--- src/_data/squash.js
+++ src/_data/squash.js
@@ -1,6 +1,12 @@
 module.exports = {
   description:
     'Founded in 1970, West Warwicks Squash is a thriving hub of activity for all ages and abilities. We run monthly box leagues for all players, several weekly club social sessions and quarterly tournaments. One of only 5 England Squash Performance Hubs in the UK we host the world renowned Rob Owen Academy (ROA). The alumni of the club and ROA include legend of Squash, Jonah Barrington, 6 times British Open Champion, Paul Coll current World #1, Sarah-Jane Perry England Number #1 / World #5 and many junior & senior world top 50 players. ',
+  warwickshireSquashStars: 'https://www.warwickshiresquash.org/squash-stars/',
+  wwsc1stTeam:
+    'https://warwickshiresquash.leaguemaster.co.uk/cgi-county/icounty.exe/showteamfixtures?divisionid=1&teamid=38',
+  wwsc2ndTeam:
+    'https://warwickshiresquash.leaguemaster.co.uk/cgi-county/icounty.exe/showteamfixtures?divisionid=4&teamid=39',
+  warwickshireLeague: 'https://www.warwickshiresquash.org/',
   coach: {
     name: 'Ali Jafari',
     email: 'ali@westwarwicks.co.uk',
@@ -45,6 +51,48 @@ module.exports = {
       name: 'campaign against living miserably',
       website: '',
       logo: '/files/calm-logo.svg',
+    },
+  ],
+  committee: [
+    {
+      name: 'Jamie Roberts',
+      role: 'Chairman',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Mike Hawkswood',
+      role: 'Treasurer',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Daniel Suarez-Mash',
+      role: 'Member',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Duncan Campbell',
+      role: 'Member',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'David Harper',
+      role: 'Member',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Mick Mulroy',
+      role: 'Member',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Ali Jafari',
+      role: 'Member',
+      image: '/files/person-circle.svg',
+    },
+    {
+      name: 'Andy Stevenson',
+      role: 'Member',
+      image: '/files/me.webp',
     },
   ],
 }
