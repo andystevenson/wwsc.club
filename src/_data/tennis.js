9ec75f1579d78d48b2d9fe6044a408bdee4adee5
andystevenson <andystevenson@mac.com>
2022-03-09 22:44:10 +0000

    populated tennis section

---
+++ src/_data/tennis.js
@@ -0,0 +1,107 @@
+module.exports = {
+  name: 'tennis',
+  description:
+    'West Warwicks Tennis Club can trace its origins back to 1888 from the amalgamation of Moseley LTC (1882), Newton LTC (1886) and Olton LTC (1898). We welcome players of all ages and standard. Our new coaching partners "All Court Tennis" provide an extensive set of coaching for all age groups. Alumni of our Tennis Club include Dan Evans British #1 and Highest World Ranking of #22.',
+  clubspark: 'https://clubspark.lta.org.uk/WestWarwicks/Coaching',
+
+  coaches: {
+    name: 'All Court Tennis',
+    description:
+      'All Court Tennis  has a number of qualified, DBS, safeguarding in children & first aid trained tennis coaches, who provide both group and private coaching. Our experienced team are friendly and professional so please get in touch if you have a question or would like to book a session.',
+    members: [
+      {
+        name: 'Matt Tully',
+        title: 'Head Coach',
+        image: '/files/matt-tully.avif',
+        bio: 'Matt graduated with a business finance degree from West Liberty University in America on a tennis scholarship in 2017. He is an LTA Level 4 qualified tennis coach with over 10 years of coaching experience. Matt has played nationally and internationally in tournaments and has moved across to West Warwicks after 5 years coaching at Solihull Arden Club. Matt also captains and leads sessions for 7 county teams from Hereford & Worcestershire at Manor Park Sports Club in Malvern and is a 10&U national pathway coach. Matt is excited to be building up a team of coaches to produce an excellent development and performance programme for both juniors and adults at the club and is available for both group and individual coaching.',
+        telephone: '07724521559',
+        email: 'tennis@westwarwicks.co.uk',
+      },
+      {
+        name: 'Matt Corp',
+        title: 'Head Coach',
+        image: '/files/matt-corp.jpg',
+        bio: 'Matt graduated with a degree in Sports Studies in 2007, he is a LTA level 3 & PTR level 4 qualified tennis coach with over 17 years experience. Matt is passionate about getting more people involved in sport and and getting people active.  In 2014 after 10 years  service as Head coach at Solihull Tennis Club made the decision to work alongside Paul Murtagh (Walking Football Chairman) to take on Beechcroft. Matt is head coach at West Warwickshire Sports Complex and is focused on developing the tennis programme, working with the coaching team coaching groups and also offers private coaching.',
+        telephone: '07930164811',
+        email: 'tennis@westwarwicks.co.uk',
+      },
+      {
+        name: 'Alex Navarro',
+        title: 'Club Coach',
+        image: '/files/alex-navarro.jpeg',
+        bio: 'Alex has over 20 years playing experience, reaching top 500 in Spain and has been coaching for over 10 years in Spain,  Croatia, London and now Birmingham. Alex is a level 3 RPT qualified tennis coach. His coaching approach is suitable for all levels from beginners to pros and his passion for coaching tennis at all levels is exceptional whilst coaching the physical and positive mental attributes of tennis in a fun, safe environment. West Warwicks are very proud to have Alex in the team and is an important piece of where the club looking to go in the near future. Alex is offers both group and private lessons.',
+        email: 'tennis@westwarwicks.co.uk',
+      },
+    ],
+  },
+  affiliates: [
+    {
+      name: 'lawn tennis association',
+      website: 'https://www.lta.org.uk/',
+      logo: '/files/lta-logo_neg@2x.png',
+      // logo: '/files/lta-logo.svg',
+      darkBackground: '#16316f',
+    },
+    {
+      name: 'clubspark lta',
+      website: 'https://clubspark.lta.org.uk/',
+      logo: '/files/clubspark-logo.svg',
+      darkBackground: '#16316f',
+    },
+  ],
+  social: [
+    { twitter: 'https://twitter.com/WestWarwickTenn' },
+    { facebook: 'https://www.facebook.com/WestWarwicksTennisClub' },
+    { youtube: 'https://www.youtube.com/user/britishtennislta' },
+  ],
+  charities: [
+    {
+      name: 'campaign against living miserably',
+      website: '',
+      logo: '/files/calm-logo.svg',
+    },
+  ],
+
+  people: {
+    title: 'Committee',
+    email: 'tennis.committee@westwarwicks.co.uk',
+    emailMessage: 'email tennis committee',
+    members: [
+      {
+        name: 'Terry Dight',
+        role: 'Chairman',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Tim Kennedy',
+        role: 'Treasurer',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Terri Gough',
+        role: 'Secretary',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Matthew Tully',
+        role: 'Coach',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Matthew Corp',
+        role: 'Coach',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Andy Jeffs',
+        role: 'Juniors',
+        image: '/files/person-circle.svg',
+      },
+      {
+        name: 'Alison Own',
+        role: 'Ladies',
+        image: '/files/person-circle.svg',
+      },
+    ],
+  },
+}
