e08f6922982934c2084d227e40cbec83535f2be1
andystevenson <andystevenson@mac.com>
2022-03-11 01:33:01 +0000

    fleshed out fitness page

---
+++ src/_data/fitness.js
@@ -0,0 +1,76 @@
+module.exports = {
+  coaches: {
+    name: 'Fitness Team',
+    description:
+      'Larisa Arcip and Dan Spiridon are ex-professional athletes who performed at international level for their home country of Romania. They share a passion to progress the fitness and wellbeing of everyone who visits their classes at West Warwicks.',
+    members: [
+      {
+        name: 'Laris Arcip',
+        title: 'Head of Health & Fitness',
+        image: '/files/larisa-arcip.avif',
+        bio: 'Larisa is an ex-professional athlete and elite Personal Trainer qualified to develop members of all ages to their highest potential.',
+        telephone: '+44 7472 676672',
+        email: 'fitness@westwarwicks.co.uk',
+      },
+      {
+        name: 'Dan Spiridon',
+        title: 'Personal Trainer',
+        image: '/files/dan-spiridon.avif',
+        bio: 'Dan is an ex-professional footballer with an extensive knowledge as an exceptional Personal Trainer.',
+        telephone: '+44 7492 924542',
+        email: 'fitness@westwarwicks.co.uk',
+      },
+    ],
+  },
+  classes: [
+    {
+      name: 'personal trainer',
+      image: '/cloudinaried/600/fitness/fitness-i1.jpg',
+    },
+    {
+      name: 'yoga',
+      image: '/files/fitness-yoga.jpg',
+    },
+    {
+      name: 'circuits',
+      image: '/files/fitness-circuits.jpg',
+    },
+    {
+      name: 'fire up',
+      image: '/files/fitness-fire-up.jpg',
+    },
+    {
+      name: 'pilates',
+      image: '/files/fitness-pilates.jpg',
+    },
+    {
+      name: 'group running',
+      image: '/files/fitness-group-running.jpg',
+    },
+    {
+      name: 'zumba',
+      image: '/files/fitness-zumba.jpg',
+    },
+
+    {
+      name: 'boot camp',
+      image: '/files/fitness-boot-camp.jpg',
+    },
+    {
+      name: 'leg workout',
+      image: '/files/fitness-leg-workout.jpg',
+    },
+    {
+      name: 'fit kids',
+      image: '/cloudinaried/600/fitness/fitness-i65.jpg',
+    },
+    {
+      name: 'spinning',
+      image: '/files/fitness-spinning.jpg',
+    },
+    {
+      name: '',
+      image: '/files/fitness-insanity.png',
+    },
+  ],
+}
