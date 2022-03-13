707a2a900b03c40546f1fcc57875ba59e2b0859b
andystevenson <andystevenson@mac.com>
2022-03-13 00:27:41 +0000

    feat: top level events

---
+++ src/_data/events.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'events',
+  description:
+    'West Warwicks has several large function rooms to accomodate many different types of events. We regularly hold birthday parties, weddings, board meetings, funeral wakes, discos, quiz nights, etc. We offer open bar, waiter service and excellent food and buffet services.',
+  subject: 'Book an Event',
+  booking: 'book an event',
+  gallery: [
+    { image: '/cloudinaried/600/events/events-i15.jpg' },
+    { image: '/files/events-birthday.jpg' },
+    { image: '/files/events-funeral.jpg' },
+    { image: '/cloudinaried/600/events/events-i89.jpg' },
+    { image: '/files/events-meetings.jpg' },
+    { image: '/files/events-disco.jpg' },
+  ],
+}
---
+++ src/_data/restaurant.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'restaurant',
+  description:
+    'West Warwicks prides itself on providing excellent seasonal dishes throughout the year. Our Sunday lunch is great value for the family and widely renowned as one of the best in the West Midlands.',
+  chef: 'Robert Powell',
+  gallery: [
+    { image: '/cloudinaried/600/restaurant/restaurant-i15.jpg' },
+    { image: '/cloudinaried/600/restaurant/restaurant-i9.jpg' },
+    { image: '/cloudinaried/600/restaurant/restaurant-main.jpg' },
+    { image: '/cloudinaried/600/restaurant/restaurant-i4.jpg' },
+    { image: '/cloudinaried/600/restaurant/restaurant-i7.jpg' },
+    { image: '/cloudinaried/600/restaurant/restaurant-i10.jpg' },
+  ],
+}
