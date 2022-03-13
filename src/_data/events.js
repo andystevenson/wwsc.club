e2a5073cc206d14fbbb49d8cba89f0e007266ade
andystevenson <andystevenson@mac.com>
2022-03-13 02:26:09 +0000

    enh: tidied up booking button

--- src/_data/events.js
+++ src/_data/events.js
@@ -3,7 +3,7 @@ module.exports = {
   description:
     'West Warwicks has several large function rooms to accomodate many different types of events. We regularly hold birthday parties, weddings, board meetings, funeral wakes, discos, quiz nights, etc. We offer open bar, waiter service and excellent food and buffet services.',
   subject: 'Book an Event',
-  booking: 'book an event',
+  booking: 'Book an Event',
   gallery: [
     { image: '/cloudinaried/600/events/events-i15.jpg' },
     { image: '/files/events-birthday.jpg' },
--- src/_data/restaurant.js
+++ src/_data/restaurant.js
@@ -3,6 +3,8 @@ module.exports = {
   description:
     'West Warwicks prides itself on providing excellent seasonal dishes throughout the year. Our Sunday lunch is great value for the family and widely renowned as one of the best in the West Midlands.',
   chef: 'Robert Powell',
+  subject: 'Book a Table',
+  booking: 'Book a Table',
   gallery: [
     { image: '/cloudinaried/600/restaurant/restaurant-i15.jpg' },
     { image: '/cloudinaried/600/restaurant/restaurant-i9.jpg' },
