754d820a9f0e8b64f55004f1b2d9191ac06838fa
andystevenson <andystevenson@mac.com>
2022-09-12 13:32:36 +0100

    removed tokens

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,6 +1,4 @@
-import token from 'https://raw.githubusercontent.com/andystevenson/wwsc.club/master/src/deno/authorization-token.js'
-import validate from 'https://raw.githubusercontent.com/andystevenson/wwsc.club/master/src/deno/valid-autorization.js'
 export default async (request, { log }) => {
   await log('hello from the edge!')
-  return new Response(`hello from the edge ${token} ${validate(token)}`)
+  return new Response(`hello from the edge`)
 }
