cff39d0f7fc9011fc08da5b7e0b355a66c6ca71f
andystevenson <andystevenson@mac.com>
2022-09-13 16:08:55 +0100

    trying to get the path working again

--- netlify/edge-functions/card-check.js
+++ netlify/edge-functions/card-check.js
@@ -26,7 +26,7 @@ const find = (members, cardnumber) => {
 }
 
 const loadCache = async (url, { log }) => {
-  const path = `${url.origin}/${cache}`
+  const path = `/${cache}`
   try {
     log(`loading cache-ashbourne...`)
     // const options = {headers: { authorization: token }}
