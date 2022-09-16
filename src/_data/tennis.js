c0784e0180db119968bbe028fcada94e0256550c
andystevenson <andystevenson@mac.com>
2022-09-16 13:11:06 +0100

    finalised migration from cloudinary

--- src/_data/tennis.js
+++ src/_data/tennis.js
@@ -5,12 +5,30 @@ module.exports = {
   clubspark: 'https://clubspark.lta.org.uk/WestWarwicks/Coaching',
 
   gallery: [
-    { image: '/cloudinaried/600/tennis/tennis-i1.jpg' },
-    { image: '/cloudinaried/600/tennis/tennis-i18.jpg' },
-    { image: '/cloudinaried/600/tennis/tennis-i3.jpg' },
-    { image: '/cloudinaried/600/tennis/tennis-i40.jpg' },
-    { image: '/cloudinaried/600/tennis/tennis-i5.jpg' },
-    { image: '/cloudinaried/600/tennis/tennis-i14.jpg' },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/4Kw7DNeRXYNBFovFOVBM6G/8b0656db9c89051f78a8169258b254ed/tennis-courts-sunny-day.webp',
+    },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/fHjH6tQCR3OZiuxHxC2l8/bbb4e949dc29549b4f337f4d1368144f/tennis-i18.webp',
+    },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/6dmLS5paHA0A845t4ijAc4/c6ba0b821c0129dce45731430ae842d6/tennis-i3.webp',
+    },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/4Kw7DNeRXYNBFovFOVBM6G/8b0656db9c89051f78a8169258b254ed/tennis-courts-sunny-day.webp',
+    },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/59VKSsYoUNoxF8o2fQchQC/5b62e089e6486471c091560f82dfe25c/tennis-adult-doubles.webp',
+    },
+    {
+      image:
+        'https://images.ctfassets.net/ffrbyg3cfykl/4Ba4xB4XDEKKgLTNAkuvv9/9050ca4eb38305c25909b352ed3d4e37/tennis-i14.webp',
+    },
   ],
   coaches: {
     name: 'All Court Tennis',
@@ -20,7 +38,8 @@ module.exports = {
       {
         name: 'Matt Tully',
         title: 'Head Coach',
-        image: '/cloudinaried/600/tennis/tennis-matt-tully.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/6DbHrusIIuI4XkoaUkJb04/25c7a2a4a860e5a9def5dd550afc0af4/tennis-matt-tully.webp',
         bio: 'Matt graduated with a business finance degree from West Liberty University in America on a tennis scholarship in 2017. He is an LTA Level 4 qualified tennis coach with over 10 years of coaching experience. Matt has played nationally and internationally in tournaments and has moved across to West Warwicks after 5 years coaching at Solihull Arden Club. Matt also captains and leads sessions for 7 county teams from Hereford & Worcestershire at Manor Park Sports Club in Malvern and is a 10&U national pathway coach. Matt is excited to be building up a team of coaches to produce an excellent development and performance programme for both juniors and adults at the club and is available for both group and individual coaching.',
         telephone: '07724521559',
         email: 'tennis@westwarwicks.co.uk',
@@ -28,7 +47,8 @@ module.exports = {
       {
         name: 'Matt Corp',
         title: 'Head Coach',
-        image: '/cloudinaried/tennis/tennis-matt-corp.jpg',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/7u96zjEIbTjA73BEHxmMHs/f6a03f2472fcb3883ef2eb4e1b507897/tennis-matt-corp.webp',
         bio: 'Matt graduated with a degree in Sports Studies in 2007, he is a LTA level 3 & PTR level 4 qualified tennis coach with over 17 years experience. Matt is passionate about getting more people involved in sport and and getting people active.  In 2014 after 10 years  service as Head coach at Solihull Tennis Club made the decision to work alongside Paul Murtagh (Walking Football Chairman) to take on Beechcroft. Matt is head coach at West Warwickshire Sports Complex and is focused on developing the tennis programme, working with the coaching team coaching groups and also offers private coaching.',
         telephone: '07930164811',
         email: 'tennis@westwarwicks.co.uk',
@@ -36,7 +56,8 @@ module.exports = {
       {
         name: 'Alex Navarro',
         title: 'Club Coach',
-        image: '/cloudinaried/tennis/tennis-alex-navarro.jpeg',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/1lQSwHB348YTlchXi0piB7/f6725dd2e36d4a139adc6fa48e945e86/tennis-alex-navarro.webp',
         bio: 'Alex has over 20 years playing experience, reaching top 500 in Spain and has been coaching for over 10 years in Spain,  Croatia, London and now Birmingham. Alex is a level 3 RPT qualified tennis coach. His coaching approach is suitable for all levels from beginners to pros and his passion for coaching tennis at all levels is exceptional whilst coaching the physical and positive mental attributes of tennis in a fun, safe environment. West Warwicks are very proud to have Alex in the team and is an important piece of where the club looking to go in the near future. Alex is offers both group and private lessons.',
         email: 'tennis@westwarwicks.co.uk',
       },
@@ -46,7 +67,7 @@ module.exports = {
     {
       name: 'lawn tennis association',
       website: 'https://www.lta.org.uk/',
-      logo: '/cloudinaried/tennis/tennis-lta-logo_neg2x.png',
+      logo: 'https://images.ctfassets.net/ffrbyg3cfykl/LRL8O9pGFWUlMSTf5THNW/3241aeb14a29c0b2186269710d5b8669/tennis-lta-logo.webp',
       darkBackground: '#16316f',
     },
     {
@@ -95,37 +116,44 @@ module.exports = {
       {
         name: 'Terry Dight',
         role: 'Chairman',
-        image: '/cloudinaried/tennis/tennis-terry-dight.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/6o3etzs2mIjG9R4Ayq51YN/fef513c968682585c437353fef77c0ef/tennis-terry-dight.webp',
       },
       {
         name: 'Tim Kennedy',
         role: 'Treasurer',
-        image: '/cloudinaried/tennis/tennis-tim-kennedy.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/XiTdq14uIKtJ8AUvGzpIh/fbbe12248d3beb64b8e6e0788d607e8a/tennis-tim-kennedy.webp',
       },
       {
         name: 'Terri Gough',
         role: 'Secretary',
-        image: '/cloudinaried/tennis/tennis-terri-gough.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/2plxVfCqbb4UYsJm6taj26/c7c8205f6d06f3da2748061a22c4fda1/tennis-terri-gough.webp',
       },
       {
         name: 'Matthew Tully',
         role: 'Coach',
-        image: '/cloudinaried/tennis/tennis-matt-tully-headshot.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/7zAmm1xEhASTVzCjxO4ybf/ce2926cb46878aacdc64839678b02341/tennis-matt-tully-headshot.webp',
       },
       {
         name: 'Matthew Corp',
         role: 'Coach',
-        image: '/cloudinaried/tennis/tennis-matt-corp-headshot.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/5Vwr42EoFAOwP7gkMP10v1/7f5c8b8f2226b2bab90a777659219cc2/tennis-matt-corp-headshot.webp',
       },
       {
         name: 'Andy Jeffs',
         role: 'Juniors',
-        image: '/cloudinaried/tennis/tennis-andy-jeffs.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/2X456vNvrXW2iudRISErKD/0a530e58e7de438e66d293fefc83c62d/tennis-andy-jeffs.webp',
       },
       {
         name: 'Alison Owen',
         role: 'Ladies',
-        image: '/cloudinaried/tennis/tennis-alison-owen.avif',
+        image:
+          'https://images.ctfassets.net/ffrbyg3cfykl/7592nEiSWYJro45H3IPYgr/f0d81093595e990ffffa69e147afcf4b/tennis-alison-owen.webp',
       },
     ],
   },
