bf1f5f55f99b81fe9e01482d9971504bbd196b60
Andy Stevenson <andystevenson@mac.com>
2023-11-21 19:37:23 +0000

    added new year eve 2023

--- src/_data/tickets.js
+++ src/_data/tickets.js
@@ -3,4 +3,5 @@ module.exports = {
   'christmas-party-kids-2023': 'https://buy.stripe.com/5kA3fC1Kzg34c3C9DF',
   'christmas-bap-and-bop-2023': 'https://buy.stripe.com/aEU4jG3SH18a6Ji2be',
   'michael-buble-tribute-2023': 'https://buy.stripe.com/3cs8zW1Kz7wy9VudTX',
+  'new-year-eve-party-2023': 'https://buy.stripe.com/7sIg2o3SHeZ0aZydU3',
 }
